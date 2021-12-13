c910210a63b89fb2ae62f15cd7162dda9f365eac
gchen <gchen3544@gmail.com>
2021-12-13 14:23:06 +0800

    文档

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cmd_update=`git pull`
+cmd_update=$(git pull)
 
 if [ "$cmd_update" != "Already up-to-date." ]; then
     chmod 0755 "./rc_wlink_service_v2"
 fi
 
-ID=`ps -ef | grep "./rc_wlink_service_v2" | grep -v "grep" | awk '{print $2}'`
+ID=$(ps -ef | grep "./rc_wlink_service_v2" | grep -v "grep" | awk '{print $2}')
 if [ $ID ]; then
     kill $ID
 fi
