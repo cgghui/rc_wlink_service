993496b2dade164d75cb42bf6ea9a987831f9c19
gchen <gchen3544@gmail.com>
2021-12-16 16:24:36 +0800

    run

--- run.sh
+++ run.sh
@@ -11,11 +11,22 @@ git pull
 chmod 0755 "./rc_wlink_service_v2"
 chmod 0755 "./bt_firewall/bt_firewall"
 
+# shellcheck disable=SC2009
 ID=$(ps -ef | grep "./rc_wlink_service_v2" | grep -v "grep" | awk '{print $2}')
-if [ $ID ]; then
-    kill $ID
+if [ "$ID" ]; then
+    kill "$ID"
 fi
 
 nohup "./rc_wlink_service_v2" &
+
+cd "./bt_firewall/" || exit
+
+# shellcheck disable=SC2009
+ID=$(ps -ef | grep "./bt_firewall" | grep -v "grep" | awk '{print $2}')
+if [ "$ID" ]; then
+    kill "$ID"
+fi
+
+nohup "./bt_firewall" &
 
 echo "Done."
