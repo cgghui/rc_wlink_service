3b953ba9236138d9fc1d714912ab63f4136c9c0c
gchen <gchen3544@gmail.com>
2021-12-17 14:19:35 +0800

    update

--- run.sh
+++ run.sh
@@ -9,12 +9,10 @@ if [ "$ID" ]; then
     kill "$ID"
     echo "kill rc_wlink_service_v2 id: $ID"
 fi
-
 nohup "./rc_wlink_service_v2" >> ./log.log 2>&1 &
 echo "rc_wlink_service_v2 start success"
 
 cd "./bt_firewall/" || exit
-
 # shellcheck disable=SC2009
 ID=$(ps -ef | grep "./bt_firewall" | grep -v "grep" | awk '{print $2}')
 if [ "$ID" ]; then
