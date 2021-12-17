dc1f0c5378fe30982fc7b2a748faa2bd98c81b7c
gchen <gchen3544@gmail.com>
2021-12-17 14:07:25 +0800

    update

--- run.sh
+++ run.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#cmd_update=$(git pull)
-
-#if [ "$cmd_update" != "Already up-to-date." ]; then
-#    chmod 0755 "./rc_wlink_service_v2"
-#fi
-
-git pull
-
 chmod 0755 "./rc_wlink_service_v2"
 chmod 0755 "./bt_firewall/bt_firewall"
 
