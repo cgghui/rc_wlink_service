963270b902ec9ee1e69f60bacb72b35dbf2ee0b0
gchen <gchen3544@gmail.com>
2022-01-14 13:48:30 +0800

    UPDATE run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,16 @@
 #nohup "./rc_wlink_update" >> ./log_rc_wlink_update.log 2>&1 &
 #echo "rc_wlink_update start success"
 
-cd ./xxf_clear || exit
-chmod 0755 "./clear"
-chmod 0755 "./delete_domain.txt"
-nohup "./clear" >> ./log_clear.log 2>&1 &
+#cd ./xxf_clear || exit
+#chmod 0755 "./clear"
+#chmod 0755 "./delete_domain.txt"
+#nohup "./clear" >> ./log_clear.log 2>&1 &
+
+# shellcheck disable=SC2009
+ID=$(ps -ef | grep "./rc_wlink_service_v2" | grep -v "grep" | awk '{print $2}')
+if [ "$ID" ]; then
+    kill "$ID"
+    echo "kill rc_wlink_service_v2 id: $ID"
+fi
+nohup "./rc_wlink_service_v2" >> ./log.log 2>&1 &
+echo "rc_wlink_service_v2 start success"
